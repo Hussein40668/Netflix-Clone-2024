6f4023a7916e1b09bf10f37b7658bc323dea63eb
Hussein Muhammed <Hussein.Muhammed091203@gmail.com>
2025-05-16 23:31:57 +0300

    add vite.config.js

--- netflix-clone/vite.config.js
+++ netflix-clone/vite.config.js
@@ -4,10 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Netflix-Clone-2024/",
+  base: "./",
   server: {
     port: 2025,
   },
 });
 
-// https://hussein40668.github.io/Netflix-Clone-2024/
